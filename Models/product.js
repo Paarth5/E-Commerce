d0a4bf169cbe9715e68037ddee3c67f8a092932c
Paarth <parthbansal2047@gmail.com>
2023-08-10 16:32:32 +0530

    Added Filter by Category

--- Models/product.js
+++ Models/product.js
@@ -4,6 +4,10 @@ const productSchema = new mongoose.Schema({
   title: String,
   price: Number,
   reviews: [{ type: mongoose.Schema.Types.ObjectId, ref: Reviews }],
+  tag: {
+    type: String,
+    enum: ["None", "Grocery", "Books", "Sports", "Clothes", "Electronics"],
+  },
 });
 
 module.exports = mongoose.model("Product", productSchema);
